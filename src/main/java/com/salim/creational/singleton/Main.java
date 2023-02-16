a194d75561a913c700e0b7ebf123ed9433781b14
Ahmad Al Sabsabi <ahmadalsabsabii@gmail.com>
2023-02-16 19:00:56 +0200

    fixed version of read me

--- src/main/java/com/salim/creational/singleton/Main.java
+++ src/main/java/com/salim/creational/singleton/Main.java
@@ -17,7 +17,8 @@ We have created a singleton class Database.
 The dbObject is a class type field. This will refer to the object of the class Database.
 The private constructor Database() prevents object creation outside of the class.
 The static class type method getInstance() returns the instance of the class to the outside world.
-In the Main class, we have class type variable db1. We are calling getInstance() using db1 to get the only object of the Database.
+In the Main class, we have class type variable db1. We are calling getInstance() using db1 to get the only object of
+the Database.
 The method getConnection() can only be accessed using the object of the Database.
 Since the Database can have only one object, all the clients can access the database through a single connection.
 */
